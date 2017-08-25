7b739cd18378b1c8cd336de1a617edaa3ca6c8cb
Josh Paul <jpaul@wearebond.com>
2017-08-24 19:00:23 -0700

    Include js in main.,

--- src/app/main.js
+++ src/app/main.js
@@ -1,5 +1,28 @@
-console.log('main.js')
-jQuery('document').ready(function(){
-  console.log('ready');
-});
+$(document).ready(function() {
+  $(window).scroll(function() {
+    if (window.pageYOffset >= 67) {
+      $('#top-nav').hide();
+      $('.ham-menu-home').show();
+    }
+    if (window.pageYOffset < 67) {
+      $('#top-nav').show();
+      $('.ham-menu-home').hide();
+    }
+  })
+  $(".scroll-arrow").click(function() {
+    window.scrollTo(0, $('#header-home').height());
+  });
 
+  $('.contact-close').click(function() {
+    $('.contact-us-modal').fadeToggle();
+  });
+
+  $('.contact-us-link').click(function() {
+    $('.contact-us-modal').fadeIn();
+    $('.nav-modal').fadeOut();
+  });
+
+  $('.ham-menu, .menu-close').click(function(){
+    $('.nav-modal').fadeToggle();
+  });
+})
