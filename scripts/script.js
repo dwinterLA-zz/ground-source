a302970c008b7ea377dc5fb92a00b6b3f6148909
matt+groundsource@wearebond.com <matt+groundsource@wearebond.com>
2017-10-03 19:25:29 +0000

    Deploy from Forestry.io

--- scripts/script.js
+++ scripts/script.js
@@ -7,8 +7,7 @@ var PASSWORDS = ["keller+groundsource2017"];
 
 $(document).ready(function () {
   if (!compatibleBrowser()) {
-    flexibility(document);
-    $('.contact-info-body').hide();
+    // show labels on ie9 because it doesnt support placeholder text
     $('.contact-labels').show();
   }
   authenticate();
@@ -236,6 +235,7 @@ function addScrollListener() {
 }
 
 function authenticate() {
+  $("#dialog").css("opacity", 1);
   var storeCredentials = localStorage.getItem("credentials");
 
   if (checkCredentials(storeCredentials)) {
@@ -253,6 +253,13 @@ function checkCredentials(credentials) {
 
 function displaySite() {
   $('.main').show();
+  $('.review-carousel').slick({
+    "autoplay": false,
+    "draggable": false,
+    "dots": true,
+    "prevArrow": $('.navigation-arrow-left'),
+    "nextArrow": $('.navigation-arrow-right')
+  });
 }
 
 function login() {
