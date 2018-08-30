57291c760f92942b6adb187a151e8ebc6ebb2773
Josh Paul <jpaul@wearebond.com>
2018-08-30 15:51:47 -0700

    Adds some simple logic for controlling the available states of experience page (tenant, landlord, and investment).

---
+++ scripts/experience.js
@@ -0,0 +1,50 @@
+$(document).ready(function() {
+  stateFromQuery()
+  setClickHandlers()
+
+  function stateFromQuery() {
+    var query = (location.search.match(new RegExp('service' + "=(.*?)($|\&)", "i")) || [])[1]
+    switch (query) {
+      case 'tenant':
+        setState('.state--tenant')
+        break;
+      case 'landlord':
+        setState('.state--landlord')
+        break;
+      case 'investment':
+        setState('.state--investment')
+        break;
+      default:
+        // TODO: create a default state? But what would we place on the bottom?
+        setState('.state--tenant')
+        console.log('none found, settting to tenant?')
+    }
+
+  }
+
+  function setState(state) {
+    setActiveLink(state)
+    $('.state').hide()
+    $(state).show()
+  }
+
+  function setActiveLink(state) {
+    $('.state__link').removeClass('active')
+    $(state + '.state__link').addClass('active')
+  }
+
+  function setClickHandlers() {
+    $('.state__link').on('click', function(event) {
+      event.preventDefault()
+    })
+    $('#tenant-service-link').on('click', function() {
+      setState('.state--tenant')
+    })
+    $('#landlord-service-link').on('click', function() {
+      setState('.state--landlord')
+    })
+    $('#investment-service-link').on('click', function() {
+      setState('.state--investment')
+    })
+  }
+})
