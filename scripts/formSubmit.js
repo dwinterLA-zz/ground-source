440efc43f556bd6ffcc0c0772c134043e2822fee
Josh Paul <jpaul@wearebond.com>
2019-04-25 12:07:35 -0700

    Properly handle success / fail AJAX requests.

--- scripts/formSubmit.js
+++ scripts/formSubmit.js
@@ -1,11 +1,12 @@
-
-function validEmail(email) { // see:
+function validEmail(email) {
+  // see:
   var re = /^([\w-]+(?:\.[\w-]+)*)@((?:[\w-]+\.)*\w[\w-]{0,66})\.([a-z]{2,6}(?:\.[a-z]{2})?)$/i;
   return re.test(email);
 }
 
 function validateHuman(honeypot) {
-  if (honeypot) {  //if hidden form filled up
+  if (honeypot) {
+    //if hidden form filled up
     console.log("Robot Detected!");
     return true;
   } else {
@@ -16,32 +17,35 @@ function validateHuman(honeypot) {
 // get all data in form and return object
 function getFormData() {
   var elements = document.getElementById("gform").elements; // all form elements
-  var fields = Object.keys(elements).map(function(k) {
-    if(elements[k].name !== undefined) {
-      return elements[k].name;
-    // special case for Edge's html collection
-    }else if(elements[k].length > 0){
-      return elements[k].item(0).name;
-    }
-  }).filter(function(item, pos, self) {
-    return self.indexOf(item) == pos && item;
-  });
+  var fields = Object.keys(elements)
+    .map(function(k) {
+      if (elements[k].name !== undefined) {
+        return elements[k].name;
+        // special case for Edge's html collection
+      } else if (elements[k].length > 0) {
+        return elements[k].item(0).name;
+      }
+    })
+    .filter(function(item, pos, self) {
+      return self.indexOf(item) == pos && item;
+    });
   var data = {};
-  fields.forEach(function(k){
+  fields.forEach(function(k) {
     data[k] = elements[k].value;
     var str = ""; // declare empty string outside of loop to allow
-                  // it to be appended to for each item in the loop
-    if(elements[k].type === "checkbox"){ // special case for Edge's html collection
+    // it to be appended to for each item in the loop
+    if (elements[k].type === "checkbox") {
+      // special case for Edge's html collection
       str = str + elements[k].checked + ", "; // take the string and append
-                                              // the current checked value to
-                                              // the end of it, along with
-                                              // a comma and a space
+      // the current checked value to
+      // the end of it, along with
+      // a comma and a space
       data[k] = str.slice(0, -2); // remove the last comma and space
-                                  // from the  string to make the output
-                                  // prettier in the spreadsheet
-    }else if(elements[k].length){
-      for(var i = 0; i < elements[k].length; i++){
-        if(elements[k].item(i).checked){
+      // from the  string to make the output
+      // prettier in the spreadsheet
+    } else if (elements[k].length) {
+      for (var i = 0; i < elements[k].length; i++) {
+        if (elements[k].item(i).checked) {
           str = str + elements[k].item(i).value + ", "; // same as above
           data[k] = str.slice(0, -2);
         }
@@ -52,46 +56,51 @@ function getFormData() {
   return data;
 }
 
-function handleFormSubmit(event) {  // handles form submit withtout any jquery
-  event.preventDefault();           // we are submitting via xhr below
-  var data = getFormData();         // get the values submitted in the form
+function handleFormSubmit(event) {
+  // handles form submit withtout any jquery
+  event.preventDefault(); // we are submitting via xhr below
+  var data = getFormData(); // get the values submitted in the form
   $("#gform").hide();
   $("#form-submit-loader").show();
-  /* OPTION: Remove this comment to enable SPAM prevention, see README.md
+  /* OPTION: Remove this comment to enable SPAM prexvention, see README.md
   if (validateHuman(data.honeypot)) {  //if form is filled, form will not be submitted
     return false;
   }
   */
 
-  if( !validEmail(data.email) ) {   // if email is not valid show error
+  if (!validEmail(data.email)) {
+    // if email is not valid show error
     $("#gform").show();
     $("#form-submit-loader").hide();
     $("#email-invalid").show();
     return false;
   } else {
-    var url = "https://script.google.com/a/groundsource.net/macros/s/AKfycbyVOqoUIi-StxIdFR2Ff2AkQ7n2rLXfk012yeIqBJGDAcjvoiOc/exec";
-    var encoded = Object.keys(data).map(function(k) {
-      return encodeURIComponent(k) + '=' + encodeURIComponent(data[k])
-  }).join('&')
+    var url =
+      "https://script.google.com/a/groundsource.net/macros/s/AKfycbyVOqoUIi-StxIdFR2Ff2AkQ7n2rLXfk012yeIqBJGDAcjvoiOc/exec";
+    var encoded = Object.keys(data)
+      .map(function(k) {
+        return encodeURIComponent(k) + "=" + encodeURIComponent(data[k]);
+      })
+      .join("&");
 
     $.ajax({
       type: "POST",
       url: url,
-      data: encoded,
-      success: function() {
-        document.getElementById('form-submit-loader').style.display = 'none';
-        document.getElementById('thankyou_message').style.display = 'block';
-      },
-      fail: function() {
-        document.getElementById('form-submit-loader').style.display = 'none';
-        document.getElementById('error_message').style.display = 'block';
-      }
+      data: encoded
     })
+      .fail(function() {
+        document.getElementById("form-submit-loader").style.display = "none";
+        document.getElementById("error_message").style.display = "block";
+      })
+      .success(function() {
+        document.getElementById("form-submit-loader").style.display = "none";
+        document.getElementById("thankyou_message").style.display = "block";
+      });
   }
 }
 function loaded() {
   // bind to the submit event of our form
-  var form = document.getElementById('gform');
+  var form = document.getElementById("gform");
   form.addEventListener("submit", handleFormSubmit, false);
-};
-document.addEventListener('DOMContentLoaded', loaded, false);
+}
+document.addEventListener("DOMContentLoaded", loaded, false);
