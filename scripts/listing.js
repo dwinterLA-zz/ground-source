cfb01a26057a42b603f1fea4d98db28c08b60e94
Josh Paul <jpaul@wearebond.com>
2019-05-06 15:13:17 -0700

    Use an external script file for lsiting js

---
+++ scripts/listing.js
@@ -0,0 +1,54 @@
+$(document).ready(function() {
+  googleMap();
+  gallery();
+  scrollReveal();
+});
+
+function googleMap() {
+  const coords = {
+    lat: parseFloat(listingMap.latitude),
+    lng: parseFloat(listingMap.longitude)
+  };
+
+  if (!coords.lat || !coords.lng) {
+    return false;
+  }
+
+  $.getScript(
+    `https://maps.googleapis.com/maps/api/js?key=${listingMap.apiKey}`,
+    () => {
+      const coords = {
+        lat: parseFloat(listingMap.latitude),
+        lng: parseFloat(listingMap.longitude)
+      };
+
+      const map = new google.maps.Map(
+        document.getElementById("listing-google-map"),
+        {
+          zoom: 12,
+          scrollwheel: false,
+          disableDefaultUI: true,
+          center: coords
+        }
+      );
+
+      new google.maps.Marker({
+        position: coords,
+        map: map
+      });
+    }
+  );
+}
+
+function gallery() {
+  $(".listing-carousel").slick({
+    prevArrow: $(".gallery-arrow-left"),
+    nextArrow: $(".gallery-arrow-right")
+  });
+}
+
+function scrollReveal() {
+  window.sr = ScrollReveal();
+  sr.reveal(".info-box");
+  sr.reveal(".img-responsive");
+}
