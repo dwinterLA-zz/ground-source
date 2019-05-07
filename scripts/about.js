5e6f3475e7528b3a7e38d128c56492350ef4ae37
Josh Paul <jpaul@wearebond.com>
2019-05-07 10:55:35 -0700

    Move inline about script into its own file

---
+++ scripts/about.js
@@ -0,0 +1,21 @@
+$(document).ready(function() {
+  bindDOM();
+  scrollReveal();
+});
+
+function bindDOM() {
+  $(".agent-modal-link").click(function() {
+    const agent = $(this).data("agent");
+
+    $("#agent-" + agent).fadeIn();
+    $("body").addClass("overflow-hidden");
+  });
+}
+
+function scrollReveal() {
+  window.sr = ScrollReveal();
+  sr.reveal(".header-image");
+  sr.reveal(".head-title");
+  sr.reveal(".about-main-text");
+  sr.reveal(".agents");
+}
