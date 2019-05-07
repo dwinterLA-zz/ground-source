0c8e12405040c60e8bf7c648e93793dc3d47bfa2
Josh Paul <jpaul@wearebond.com>
2019-05-07 10:56:53 -0700

    style

--- scripts/experience.js
+++ scripts/experience.js
@@ -2,36 +2,49 @@ $(document).ready(function() {
   stateFromQuery();
   setClickHandlers();
   servicesCarousel();
+});
 
-  function stateFromQuery() {
-    var query = (location.search.match(
-      new RegExp("service" + "=(.*?)($|&)", "i")
-    ) || [])[1];
-    switch (query) {
-      case undefined:
-        setState(".state--default");
-        break;
-      case "tenant":
-        setState(".state--tenant");
-        break;
-      case "landlord":
-        setState(".state--landlord");
-        break;
-      case "investment":
-        setState(".state--investment");
-        break;
-      default:
-        setState(".state--default");
-    }
+function stateFromQuery() {
+  var query = (location.search.match(
+    new RegExp("service" + "=(.*?)($|&)", "i")
+  ) || [])[1];
+  switch (query) {
+    case undefined:
+      setState(".state--default");
+      break;
+    case "tenant":
+      setState(".state--tenant");
+      break;
+    case "landlord":
+      setState(".state--landlord");
+      break;
+    case "investment":
+      setState(".state--investment");
+      break;
+    default:
+      setState(".state--default");
   }
+}
 
-  function setState(state) {
-    setActiveLink(state);
-    $(state).addClass("active");
-    setupCarousel();
-  }
+function setState(state) {
+  setActiveLink(state);
+  $(state).addClass("active");
+  setupCarousel();
+}
 
-  function servicesCarousel() {
+function servicesCarousel() {
+  $(".services-carousel").slick({
+    autoplay: false,
+    draggable: false,
+    dots: true,
+    prevArrow: false,
+    nextArrow: false
+  });
+}
+
+function setupCarousel() {
+  if ($(".services-carousel:visible").length > 0) {
+    $(".services-carousel").slick("unslick");
     $(".services-carousel").slick({
       autoplay: false,
       draggable: false,
@@ -40,52 +53,39 @@ $(document).ready(function() {
       nextArrow: false
     });
   }
+}
+function setActiveLink(state) {
+  $(".state").removeClass("active");
+  $(".state__link").removeClass("active");
+  $(state + ".state__link").addClass("active");
+}
 
-  function setupCarousel() {
-    if ($(".services-carousel:visible").length > 0) {
-      $(".services-carousel").slick("unslick");
-      $(".services-carousel").slick({
-        autoplay: false,
-        draggable: false,
-        dots: true,
-        prevArrow: false,
-        nextArrow: false
-      });
-    }
-  }
-  function setActiveLink(state) {
-    $(".state").removeClass("active");
-    $(".state__link").removeClass("active");
-    $(state + ".state__link").addClass("active");
-  }
+function setClickHandlers() {
+  $(".state__link").on("click", function(event) {
+    event.preventDefault();
+  });
+  $("#experience-default-link").on("click", function() {
+    updateQueryParam();
+    setState(".state--default");
+  });
+  $("#tenant-service-link").on("click", function() {
+    updateQueryParam("tenant");
+    setState(".state--tenant");
+  });
+  $("#landlord-service-link").on("click", function() {
+    updateQueryParam("landlord");
+    setState(".state--landlord");
+  });
+  $("#investment-service-link").on("click", function() {
+    updateQueryParam("investment");
+    setState(".state--investment");
+  });
+}
 
-  function setClickHandlers() {
-    $(".state__link").on("click", function(event) {
-      event.preventDefault();
-    });
-    $("#experience-default-link").on("click", function() {
-      updateQueryParam();
-      setState(".state--default");
-    });
-    $("#tenant-service-link").on("click", function() {
-      updateQueryParam("tenant");
-      setState(".state--tenant");
-    });
-    $("#landlord-service-link").on("click", function() {
-      updateQueryParam("landlord");
-      setState(".state--landlord");
-    });
-    $("#investment-service-link").on("click", function() {
-      updateQueryParam("investment");
-      setState(".state--investment");
-    });
+function updateQueryParam(state) {
+  if (state) {
+    history.pushState(null, "", "/experience.html?service=" + state);
+  } else {
+    history.pushState(null, "", "/experience.html");
   }
-
-  function updateQueryParam(state) {
-    if (state) {
-      history.pushState(null, "", "/experience.html?service=" + state);
-    } else {
-      history.pushState(null, "", "/experience.html");
-    }
-  }
-});
+}
